c97037f85b67dfc6f577594e7d5e539585f92c12
Augusto Alves <augusto.malves@yahoo.com.br>
2021-07-27 01:16:30 -0300

    add the coment from where the text script was taken

--- text.js
+++ text.js
@@ -1,13 +1,14 @@
+//text scrip from https://www.w3schools.com/howto/howto_js_typewriter.asp
 var i = 0;
-var txt = "Nullam quis cursus neque, finibus ultrices leo. Donec in nisl iaculis, sodales ipsum sit amet, sagittis sem. Integer placerat risus ac magna feugiat, id placerat velit congue. Nullam vel sapien sit amet arcu congue porta. Aliquam ac cursus libero. Donec tincidunt risus sed dictum lacinia. Vivamus quis nibh in erat congue consequat eget at libero. Maecenas et sapien justo.";
+var text = "Nullam quis cursus neque, finibus ultrices leo. Donec in nisl iaculis, sodales ipsum sit amet, sagittis sem. Integer placerat risus ac magna feugiat, id placerat velit congue. Nullam vel sapien sit amet arcu congue porta. Aliquam ac cursus libero. Donec tincidunt risus sed dictum lacinia. Vivamus quis nibh in erat congue consequat eget at libero. Maecenas et sapien justo.";
 var speed = 50;
 
 function textAnimate() {
-  if (i < txt.length){
-    document.getElementById("content").innerHTML += txt.charAt(i);
+  if (i < text.length){
+    document.getElementById("content").innerHTML += text.charAt(i);
     i++;
     setTimeout(textAnimate, speed)
   }
 }
 
-textAnimate(txt)
+textAnimate(text)
