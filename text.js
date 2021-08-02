14274b5ab6a1b3fad58654d46fd2d22f20122052
Augusto Alves <augusto.malves@yahoo.com.br>
2021-08-01 22:40:09 -0300

    add icons and footer

--- text.js
+++ text.js
@@ -1,7 +1,7 @@
 //text scrip from https://www.w3schools.com/howto/howto_js_typewriter.asp
 var i = 0;
 var text = "Nullam quis cursus neque, finibus ultrices leo. Donec in nisl iaculis, sodales ipsum sit amet, sagittis sem. Integer placerat risus ac magna feugiat, id placerat velit congue. Nullam vel sapien sit amet arcu congue porta. Aliquam ac cursus libero. Donec tincidunt risus sed dictum lacinia. Vivamus quis nibh in erat congue consequat eget at libero. Maecenas et sapien justo.";
-var speed = 50;
+var speed = 1;
 
 function textAnimate() {
   if (i < text.length){
